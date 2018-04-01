0077bb5b703168a4cf1d6d6d7be3c1c4b34df71e
Romain <rvallade@gmail.com>
2018-04-01 12:32:01 -0600

    modify new file generation remove filter in ERRORS tab

--- multiplexSimoaGenerator/src/main/java/com/multiplexSimoaGenerator/Generator.java
+++ multiplexSimoaGenerator/src/main/java/com/multiplexSimoaGenerator/Generator.java
@@ -18,24 +18,71 @@ import java.util.Map;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Generator {
 	private static final String MODELE_RAPPORT = "com/multiplexSimoaGenerator/neuro4plex_Model.xlsx";
-	private static final String PATH_DATA_OUTPUT = "C:/Data";
+	private static final String PATH_DATA_OUTPUT = "C:/multiplexSimoaGenerator";
 	private Map<String, List<ExcelRow>> beadPlexMap = new HashMap();
+	private List<ExcelRow> errorRows = new ArrayList<ExcelRow>();
 
 	public void execute() throws IOException {
 		readInputFileAndStoreObjects();
 	}
-
+	
 	private void readInputFileAndStoreObjects() throws IOException {
-		FileInputStream file = new FileInputStream(new File("C:/Temp/input data.xlsx"));
+		Map<Integer, List<String>> data = new HashMap();
+		FileInputStream inputfile = new FileInputStream(new File("C:/multiplexSimoaGenerator/input_data.xlsx"));
+		
+		// create empty result file
+		String filename = getFilename();
+		FileInputStream outputStream = buildExcel(filename);
+		
+		// read input file and build the beadPlex map
+		buildBeadPlexMapFromInputFile(inputfile);
+		System.out.println("Total Number of BeadPlex found: " + beadPlexMap.keySet().size());
+		
+		// based on the map, create the tabs and fill them
+		// for each beadPlex ==> 1 tab
+		XSSFWorkbook wb = new XSSFWorkbook(outputStream);
+		for (String key : beadPlexMap.keySet()) {
+			XSSFSheet sheet = wb.cloneSheet(0, key);
+
+			/*for (int r = 0; r < 5; r++) {
+				XSSFRow row = sheet.createRow(r);
+
+				for (int c = 0; c < 5; c++) {
+					XSSFCell cell = row.createCell(c);
+
+					cell.setCellValue("Cell " + r + " " + c);
+				}
+			}*/
+		}
+		wb.removeSheetAt(0);
+		wb.setSheetOrder("ERRORS", wb.getNumberOfSheets()-1);
+		wb.setActiveSheet(0);
+		// add errors
+		XSSFSheet errorSheet = wb.getSheet("ERRORS");
+		int i = 1;
+		for (ExcelRow currentRow : errorRows) {
+			XSSFRow row = errorSheet.createRow(i++);
+			XSSFCell cell = row.createCell(0);
+			cell.setCellValue(currentRow.getErrorMessage());
+		}
+		FileOutputStream fileOut = new FileOutputStream(filename);
+
+		wb.write(fileOut);
+		fileOut.flush();
+		fileOut.close();
+		
+	}
+	
+	private void buildBeadPlexMapFromInputFile(FileInputStream file) throws IOException {
 		Workbook workbook = new XSSFWorkbook(file);
 		Sheet sheet = workbook.getSheetAt(0);
-
-		Map<Integer, List<String>> data = new HashMap();
 		int i = 0;
 		for (Row row : sheet) {
 			try {
@@ -64,11 +111,25 @@ public class Generator {
 
 						System.out.println(currentRow.toString());
 					} else {
+						currentRow.setErrorMessage(SheetUtil.getCellStringValue(row.getCell(SheetUtil.ERROR_TXT)));
+						errorRows.add(currentRow);
 						System.out.println("no AEB on line: " + (i + 1) + ", error message: "
 								+ SheetUtil.getCellStringValue(row.getCell(SheetUtil.ERROR_TXT)));
 					}
 				}
 			} catch (Exception e) {
+				String beadPlex = SheetUtil.getCellStringValue(row.getCell(SheetUtil.BEAD_PLEX_NAME));
+				String aeb = SheetUtil.getCellStringValue(row.getCell(SheetUtil.AEB));
+
+				Location location = new Location(SheetUtil.getCellStringValue(row.getCell(SheetUtil.LOCATION)));
+				ExcelRow currentRow = new ExcelRow(
+						i + 1, 
+						beadPlex, 
+						SheetUtil.getCellStringValue(row.getCell(SheetUtil.SAMPLE_ID)),
+						SheetUtil.getCellStringValue(row.getCell(SheetUtil.CONCENTRATION)), 
+						location, 
+						aeb);
+				errorRows.add(currentRow);
 				System.out.println("Error on line: " + (i + 1));
 				System.out.println(e);
 			}
@@ -91,25 +152,16 @@ public class Generator {
 				
 			});
 		}
-
-		System.out.println("Total Number of BeadPlex found: " + beadPlexMap.keySet().size());
 	}
 
-	public static void buildExcel() {
-		LocalDateTime timePoint = LocalDateTime.now();
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss");
-
-		String formatDateTime = timePoint.format(formatter);
-
-		InputStream is = Generator.class.getClassLoader()
-				.getResourceAsStream("com/multiplexSimoaGenerator/neuro4plex_Model.xlsx");
-		String fileName = "C:/Data/neuro4plex_" + formatDateTime + ".xlsx";
-		File rapportOut = new File(fileName);
+	public static FileInputStream buildExcel(String filename) throws FileNotFoundException {
+		InputStream is = Generator.class.getClassLoader().getResourceAsStream(MODELE_RAPPORT);
+		File rapportOut = new File(filename);
 		copyFile(is, rapportOut);
 
 		InputStream inputStream = null;
 		try {
-			inputStream = new FileInputStream(fileName);
+			inputStream = new FileInputStream(filename);
 		} catch (FileNotFoundException e) {
 			System.out.println("File not found in the specified path.");
 			e.printStackTrace();
@@ -122,13 +174,13 @@ public class Generator {
 			dataSheet = workBook.getSheet("DATAS");
 		} catch (Exception e) {
 			e.printStackTrace();
-			return;
+			return null;
 		}
 
 		FileOutputStream fos = null;
 		
 		try {
-			fos = new FileOutputStream(fileName);
+			fos = new FileOutputStream(filename);
 			workBook.write(fos);
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -151,6 +203,8 @@ public class Generator {
 				}
 			}
 		}
+		
+		return new FileInputStream(new File(filename));
 	}
 
 	private static int copyFile(InputStream input, File dest) {
@@ -178,9 +232,18 @@ public class Generator {
 			out.close();
 			input.close();
 		} catch (Exception e) {
-			size = -1;
 			e.printStackTrace();
 		}
 		return size;
 	}
+	
+	private String getFilename() {
+		LocalDateTime timePoint = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss");
+		String formatDateTime = timePoint.format(formatter);
+		
+		return "C:/multiplexSimoaGenerator/neuro4plex_" + formatDateTime + ".xlsx";
+	}
+
+
 }
